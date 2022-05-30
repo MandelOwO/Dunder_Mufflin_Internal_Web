76dbc6096da9431ff9bce3b541ca5a49c3bf2a77
MandelOwO <87827886+MandelOwO@users.noreply.github.com>
2022-05-30 20:16:45 +0200

    navbar and blank pages

--- employees.php
+++ employees.php
@@ -1,9 +1,9 @@
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mufflin | Home</title>
+    <title>Mufflin | Employees</title>
 
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
@@ -11,10 +11,10 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
-
 <body>
-<?php require "source/db.php" ?>
-<!-- NAVBAR -->
+
+
+    <!-- NAVBAR -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
         <div class="container">
             <a class="navbar-brand" href="index.php">Mufflin System</a>
@@ -25,10 +25,10 @@
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link " aria-current="page" href="index.php">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="employees.php">Employees</a>
+                    <li class="nav-item ">
+                        <a class="nav-link active" href="#">Employees</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="stock.php">Stock</a>
@@ -41,6 +41,7 @@
         </div>
     </nav>
 
-</body>
 
+
+</body>
 </html>
