ed1ba52e8313c0a863d44d65c4e27ffe47eef9e3
MandelOwO <87827886+MandelOwO@users.noreply.github.com>
2022-05-30 22:13:31 +0200

    directory tree, employee table

--- sales/index.php
+++ sales/index.php
@@ -3,21 +3,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mufflin | Stock</title>
+    <title>Mufflin | Sales</title>
 
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-
+<?php require '../source/db.php' ?>
 
     <!-- NAVBAR -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
         <div class="container">
-            <a class="navbar-brand" href="index.php">Mufflin System</a>
+            <a class="navbar-brand" href="../dashboard">Mufflin System</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -25,16 +28,16 @@
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link" aria-current="page" href="../dashboard"><i class="bi bi-window"></i> Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link " href="#">Employees</a>
+                        <a class="nav-link" href="../employees"><i class="bi bi-person"></i> Employees</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active"href="stock.php">Stock</a>
+                        <a class="nav-link" href="../stock"><i class="bi bi-box-seam"></i> Stock</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="sales.php">Sales</a>
+                        <a class="nav-link active" href="../sales"><i class="bi bi-cash-stack"></i> Sales</a>
                     </li>
                 </ul>
             </div>
